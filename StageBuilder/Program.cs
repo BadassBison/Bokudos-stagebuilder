5bafb052498c18c0f7d82430beb42d62dabd266d
BadassBison <shawn.scott.xd@gmail.com>
2021-01-26 09:36:30 -0600

    Start process for uploading lambda

--- StageBuilder/Program.cs
+++ StageBuilder/Program.cs
@@ -1,14 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Amazon.Lambda.Core;
 
-[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 namespace StageBuilder
 {
   public class Program
